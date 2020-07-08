b02a011233169dddddbf2e0c2935c6919e5acb7e
Mangesh Jadhav <149mpj@gmail.com>
2020-07-08 16:26:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pipe';
+  title =  'Angular 9 Project!';
+  str = 'GOOD MORNING how ArE yOU';
+  today = new Date();
+  salary = 34568;
+  months = [ 'Jan ', 'Feb', 'Mar', 'April', 'May', 'Jun', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+  data = [{id: 1, name: 'Harry', city: 'PUNE'},{ id: 2, name: 'Sumit', city: 'MUMBAI'}];
+  jsonval = {name: 'Rox', age: '25', address: {a1: 'Mumbai', a2: 'Karnataka'}};
+  jsonval1 = [ {name: 'Rox', age: '25', address: 'Pune'}, {name: 'acv', age: '22', address: 'Mumbai'},
+              {name: 'dre', age: '24', address: 'Satara'}, {name: 'wer', age: '21', address: 'Thane'}];
+  employee = [ { name: 'Harry', age: 23, gender: 'male'}, { name: 'Seema', age: 23, gender: 'female'},
+                {name: 'Sumit', age: 24, gender: 'male'}, {name: 'Kiran', age: 22, gender: 'female'} ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MypipePipe } from './mypipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MypipePipe
   ],
   imports: [
     BrowserModule
